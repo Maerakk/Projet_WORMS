96a4a88f43ee2fa4176a2ff304e93a7c2e9bdad3
Youllou <juless.barbier@gmail.com>
2021-11-24 08:26:07 +0100

    game loop copy from avoidbats

--- game.py
+++ game.py
@@ -18,11 +18,44 @@ def main():
         game_loop(window)
 
 
+
 def game_loop(window):
+    """
+    this is the game loop, as long as the user wants to play, the loops run
+    :param window: the window which the game will run in
+    """
+
+    # initializing a local gameState
+    # it will represent the current gameState
+    game_state = GameState()
+    # we draw the window the game is initialized
+    game_state.draw(window)
+    # the window isn't refreshed for now
+    # it will be at the end of each while loop
+
     quitting = False
-    gameState = GameState()
+    # this is really the game loop
     while not quitting:
-        gameState.draw(window)
+
+        # at each event we retrieve the event and analyse it
+        # this is used only for quit event
+        # for the movement we will use get_next_move
+        for event in pg.event.get():
+            # if it's a quit event (close button) then we put quit to True the we exit the loop and return
+            if event.type == pg.QUIT:
+                quitting = True
+
+        # on each loop we get the next move
+        next_move = get_next_move()
+
+        # we recalculate the game state
+        game_state.advance_state(next_move)
+
+        # and we redraw the game
+        game_state.draw(window)
         pg.display.update()
+
+        # tick
+        pg.time.delay(20)
 
 main()

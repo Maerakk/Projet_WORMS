8a802032c3e0da586da85ec6c9529b8f7b82a6d6
Maëlle Marinier <maonajma@gmail.com>
2021-11-29 02:21:14 +0100

    shot comments

--- weapons/weapon.py
+++ weapons/weapon.py
@@ -31,8 +31,12 @@ class Weapon(ABC, pg.sprite.Sprite):
     def advance_state(self, next_move):
         self.rect.x = self.player.rect.x
         self.rect.y = self.player.rect.y
+        # We check of the user is pressing the shoot button
         if next_move.shoot:
+            # If yes we put it in a variable
             self.is_shooting = True
+
+        # If the player has pressed and is not pressing the key anymore then we put in a variable that ha has shot the projectile
         if self.is_shooting and not next_move.shoot:
             self.is_shot = True
 
